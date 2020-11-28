556e71fdef7a488cbadf55554e9958313fa70004
Gabriel Pereira <gabriel_rfp@outlook.com>
2020-11-28 03:39:36 -0300

    Created Guest class

---
+++ src/main/java/com/project/hotelrecode/model/Guest.java
@@ -0,0 +1,133 @@
+package com.project.hotelrecode.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "guest")
+public class Guest {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false, unique = true)
+	private String cpf;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private String telephone;
+	
+	@Column(nullable = false)
+	private Date birthday;
+	
+	@Column(nullable = false)
+	private String profile;
+	
+	
+
+	public Guest() {
+		super();
+	}
+
+	public Guest(Long id, String name, String cpf, String email, String password, String telephone, Date birthday,
+			String profile) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.email = email;
+		this.password = password;
+		this.telephone = telephone;
+		this.birthday = birthday;
+		this.profile = profile;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	@Override
+	public String toString() {
+		return "Guest [id=" + id + ", name=" + name + ", cpf=" + cpf + ", email=" + email + ", password=" + password
+				+ ", telephone=" + telephone + ", birthday=" + birthday + ", profile=" + profile + "]";
+	}
+	
+	
+	
+
+}
