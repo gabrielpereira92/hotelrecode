474bf07fbefdfa9982dc773bda414a30a2e6ae9e
Gabriel Pereira <gabriel_rfp@outlook.com>
2020-11-28 02:36:26 -0300

    Start project

---
+++ src/main/java/com/project/hotelrecode/HotelrecodeApplication.java
@@ -0,0 +1,13 @@
+package com.project.hotelrecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelrecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelrecodeApplication.class, args);
+	}
+
+}
